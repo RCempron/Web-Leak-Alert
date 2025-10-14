c83c04edae556bdc666ae0d4ae7ac3d465c0353e
RCempron <ronaldcempronasis@gmail.com>
2025-10-14 20:41:14 +0800

    fix register form

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AppLayout from '@/components/layout/AppLayout.vue'
+import RegisterForm from '@/components/auth/RegisterForm.vue'
 import { useDisplay } from 'vuetify'
 
 const { mobile } = useDisplay()
@@ -19,39 +20,16 @@ const { mobile } = useDisplay()
               ></v-img>
               <h2 class="font-weight-black">Leak Alert</h2>
             </v-card-title>
-
             <v-card-text class="bg-surface-light pt-4">
-              <v-form fast-fail @submit.prevent>
-                <v-text-field label="Firstname" variant="outlined"></v-text-field>
+              <v-divider class="my-5"> </v-divider>
 
-                <v-text-field label="Lastname" variant="outlined"></v-text-field>
+              <RegisterForm></RegisterForm>
 
-                <v-text-field label="Email" variant="outlined"></v-text-field>
-
-                <v-text-field label="Password" type="password" variant="outlined"></v-text-field>
-
-                <v-text-field
-                  label="Password Confirmation"
-                  type="password"
-                  variant="outlined"
-                ></v-text-field>
-
-                <v-btn
-                  class="mt-2"
-                  type="submit"
-                  block
-                  color="primary"
-                  prepend-icon="mdi-account-plus"
-                  >Submit</v-btn
-                >
-
-                <v-divider class="my-5"></v-divider>
-
-                <h5 class="text-center">
-                  Already have an account?
-                  <RouterLink to="/login">Click here to Login</RouterLink>
-                </h5>
-              </v-form>
+              <v-divider class="my-5"> </v-divider>
+              <h5 class="text-center">
+                Already have an Account?
+                <RouterLink to="/login">Click here to Login</RouterLink>
+              </h5>
             </v-card-text>
           </v-card>
         </v-col>
