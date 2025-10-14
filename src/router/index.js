a3e08502c000da842684541bae3a73eedc07be59
RCempron <ronaldcempronasis@gmail.com>
2025-10-15 07:35:04 +0800

    fixed vercel deployment

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/login',
