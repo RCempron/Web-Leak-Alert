b561ef53e73b76d206204be79f4e35d8384bd65b
RCempron <ronaldcempronasis@gmail.com>
2025-10-14 20:20:02 +0800

    updated applayout

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,73 +1,61 @@
 <script setup>
-import { ref } from 'vue'
+import AppLayout from '@/components/layout/AppLayout.vue'
+import { useDisplay } from 'vuetify'
 
-const theme = ref('light')
-
-function onClick() {
-  theme.value = theme.value === 'light' ? 'dark' : 'light'
-}
+const { mobile } = useDisplay()
 </script>
 
 <template>
-  <v-responsive class="border rounded">
-    <v-app :theme="theme">
-      <v-app-bar class="px-3">
-        <v-spacer></v-spacer>
+  <AppLayout>
+    <template #content
+      ><v-row>
+        <v-col cols="12" md="6" class="mx-auto">
+          <v-card class="mx-auto" elevation="24">
+            <v-card-title class="text-center">
+              <v-img
+                class="mx-auto"
+                src="/images/LeakAlertLogo.png"
+                :width="mobile ? '75%' : '25%'"
+              ></v-img>
+              <h2 class="font-weight-black">Leak Alert</h2>
+            </v-card-title>
 
-        <v-btn
-          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-          text="Toggle Theme"
-          slim
-          @click="onClick"
-        ></v-btn>
-      </v-app-bar>
+            <v-card-text class="bg-surface-light pt-4">
+              <v-form fast-fail @submit.prevent>
+                <v-text-field label="Firstname" variant="outlined"></v-text-field>
 
-      <v-main>
-        <v-container>
-          <v-row>
-            <v-col cols="12" md="6" class="mx-auto">
-              <v-card class="mx-auto" subtitle="Register">
-                <template v-slot:title>
-                  <span class="font-weight-black">Welcome to BCWD Complaint System</span>
-                </template>
+                <v-text-field label="Lastname" variant="outlined"></v-text-field>
 
-                <v-card-text class="bg-surface-light pt-4">
-                  <v-form fast-fail @submit.prevent>
-                    <v-text-field label="Firstname" variant="outlined"></v-text-field>
+                <v-text-field label="Email" variant="outlined"></v-text-field>
 
-                    <v-text-field label="Lastname" variant="outlined"></v-text-field>
+                <v-text-field label="Password" type="password" variant="outlined"></v-text-field>
 
-                    <v-text-field label="Email" variant="outlined"></v-text-field>
+                <v-text-field
+                  label="Password Confirmation"
+                  type="password"
+                  variant="outlined"
+                ></v-text-field>
 
-                    <v-text-field
-                      label="Password"
-                      type="password"
-                      variant="outlined"
-                    ></v-text-field>
+                <v-btn
+                  class="mt-2"
+                  type="submit"
+                  block
+                  color="primary"
+                  prepend-icon="mdi-account-plus"
+                  >Submit</v-btn
+                >
 
-                    <v-text-field
-                      label="Password Confirmation"
-                      type="password"
-                      variant="outlined"
-                    ></v-text-field>
+                <v-divider class="my-5"></v-divider>
 
-                    <v-btn class="mt-2" type="submit" block>Submit</v-btn>
-
-                    <v-divider class="my-5"></v-divider>
-
-                    <h5 class="text-center">
-                      Already have an account?
-                      <RouterLink to="/login">Click here to Login</RouterLink>
-                    </h5>
-                  </v-form>
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-
-      <v-footer border app>2025 BCWD Complaint System</v-footer>
-    </v-app>
-  </v-responsive>
+                <h5 class="text-center">
+                  Already have an account?
+                  <RouterLink to="/login">Click here to Login</RouterLink>
+                </h5>
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row></template
+    >
+  </AppLayout>
 </template>
