204f26cce979d914046e3f800b25ba364deb87b5
RCempron <ronaldcempronasis@gmail.com>
2025-10-15 10:47:35 +0800

    fix login and register form

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,9 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+
+const isPasswordVisible = ref(false)
+</script>
+
 <template>
   <v-form fast-fail @submit.prevent>
-    <v-text-field label="Email" variant="outlined"></v-text-field>
+    <!-- Email -->
+    <v-text-field
+      label="Email"
+      prepend-inner-icon="mdi-email"
+      variant="outlined"
+      class="mb-3"
+    ></v-text-field>
 
-    <v-text-field label="Password" type="password" variant="outlined"></v-text-field>
+    <!-- Password -->
+    <v-text-field
+      prepend-inner-icon="mdi-lock"
+      label="Password"
+      variant="outlined"
+      :type="isPasswordVisible ? 'text' : 'password'"
+      :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+      @click:append-inner="isPasswordVisible = !isPasswordVisible"
+      class="mb-4"
+    ></v-text-field>
 
-    <v-btn class="mt-2" type="submit" block color="primary" prepend-icon="mdi-login">Login</v-btn>
+    <!-- Login Button -->
+    <v-btn class="mt-2" type="submit" block color="primary" size="large" prepend-icon="mdi-login">
+      Login
+    </v-btn>
   </v-form>
 </template>
--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -1,17 +1,69 @@
+<script setup>
+import { ref } from 'vue'
+
+// reactive variables for password visibility
+const isPasswordVisible = ref(false)
+const isConfirmPasswordVisible = ref(false)
+</script>
+
 <template>
   <v-form fast-fail @submit.prevent>
-    <v-text-field label="Firstname" variant="outlined"></v-text-field>
+    <!-- Firstname -->
+    <v-text-field
+      label="Firstname"
+      prepend-inner-icon="mdi-account"
+      variant="outlined"
+      class="mb-3"
+    ></v-text-field>
 
-    <v-text-field label="Lastname" variant="outlined"></v-text-field>
+    <!-- Lastname -->
+    <v-text-field
+      label="Lastname"
+      prepend-inner-icon="mdi-account"
+      variant="outlined"
+      class="mb-3"
+    ></v-text-field>
 
-    <v-text-field label="Email" variant="outlined"></v-text-field>
+    <!-- Email -->
+    <v-text-field
+      label="Email"
+      prepend-inner-icon="mdi-email"
+      variant="outlined"
+      class="mb-3"
+    ></v-text-field>
 
-    <v-text-field label="Password" type="password" variant="outlined"></v-text-field>
+    <!-- Password -->
+    <v-text-field
+      label="Password"
+      prepend-inner-icon="mdi-lock"
+      variant="outlined"
+      class="mb-3"
+      :type="isPasswordVisible ? 'text' : 'password'"
+      :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+      @click:append-inner="isPasswordVisible = !isPasswordVisible"
+    ></v-text-field>
 
-    <v-text-field label="Password Confirmation" type="password" variant="outlined"></v-text-field>
+    <!-- Confirm Password -->
+    <v-text-field
+      label="Confirm Password"
+      prepend-inner-icon="mdi-lock-check"
+      variant="outlined"
+      class="mb-4"
+      :type="isConfirmPasswordVisible ? 'text' : 'password'"
+      :append-inner-icon="isConfirmPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+      @click:append-inner="isConfirmPasswordVisible = !isConfirmPasswordVisible"
+    ></v-text-field>
 
-    <v-btn class="mt-2" type="submit" block color="primary" prepend-icon="mdi-account-plus"
-      >Submit</v-btn
+    <!-- Submit Button -->
+    <v-btn
+      class="mt-2"
+      type="submit"
+      block
+      color="primary"
+      size="large"
+      prepend-icon="mdi-account-plus"
     >
+      Register
+    </v-btn>
   </v-form>
 </template>
