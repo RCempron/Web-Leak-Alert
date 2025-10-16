d9aec687e9322daefb4c6e0b0e9f75e5a409142b
RCempron <ronaldcempronasis@gmail.com>
2025-10-16 08:30:39 +0800

    updated .env

--- src/components/layout/AppLayout.vue
+++ src/components/layout/AppLayout.vue
@@ -24,9 +24,7 @@ function onClick() {
       </v-app-bar>
 
       <v-main>
-        <v-container>
-          <slot name="content"></slot>
-        </v-container>
+        <slot name="content"></slot>
       </v-main>
 
       <v-footer class="font-wight-bold" border app>2025 BCWD Complaint System</v-footer>
